9cf7b495aa379904a209720d46db8b9d0a1a051b
xdauby <antoinedepaepepro@gmail.com>
2024-04-03 19:58:45 +0200

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anonymous/Documents/Bitcoin-protocol/Bitcoin-Protocol
-# Build directory: /home/anonymous/Documents/Bitcoin-protocol/Bitcoin-Protocol/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
